2c90362a8706ec89ed6e9b26ff4e82e920201f82
Alexey Fedorenko <messer.ua@gmail.com>
2020-02-09 12:25:55 +0200

    atrib directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,20 @@ import { AppComponent } from "./app.component";
 import { UsersComponent } from "./Users/users.component";
 import { CurrencyPipe } from "./pipes/currency.pipe";
 import { UpperPipe } from "./pipes/upper.pipe";
+import { BalanceColorDirective } from "./attribDirect/balanceColor.directive";
+import { ZoomDirective } from "./attribDirect/zoom.directive";
+import { ClickColorDirective } from "./attribDirect/click-color.directive";
 
 @NgModule({
-  declarations: [AppComponent, UsersComponent, CurrencyPipe, UpperPipe],
+  declarations: [
+    AppComponent,
+    UsersComponent,
+    CurrencyPipe,
+    UpperPipe,
+    BalanceColorDirective,
+    ZoomDirective,
+    ClickColorDirective
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
